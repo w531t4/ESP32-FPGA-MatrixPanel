63f63c98c045e05a43aa6652126b7781c5c9948d
Aaron White <w531t4@gmail.com>
2025-07-29 12:42:34 -0400

    move from c -> cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@
 #include "driver/spi_master.h"
 #include "driver/gpio.h"
 #include "esp_system.h"
+// #include "matrix_panel_fpga.hpp"
 #include "string.h"
+
 
 // #define PIN_NUM_MOSI 13
 #define PIN_NUM_MOSI 2
@@ -25,10 +27,20 @@ spi_device_handle_t spi;
 //         i++;
 //         vTaskDelay(5000 / portTICK_PERIOD_MS);
 //     }
+void setBrightness(spi_device_handle_t &d, uint8_t level) {
+    spi_transaction_t t = {
+        .length = 8, // bits
+        .tx_buffer = &level,
+    };
+    spi_device_transmit(d, &t); // blocking
+}
 
-void app_main(void) {
+// FPGA_SPI_CFG mxconfig_;
+// mxconfig_->
+
+extern "C" void app_main(void) {
     spi_bus_config_t buscfg = {
-        .mosi_io_num = PIN_NUM_MOSI,
+        // .mosi_io_num = mxconfig->
         .miso_io_num = -1, // Not used
         .sclk_io_num = PIN_NUM_CLK,
         .quadwp_io_num = -1,
@@ -36,16 +48,17 @@ void app_main(void) {
     };
 
     spi_device_interface_config_t devcfg = {
-        .clock_speed_hz = 2 * 1000 * 1000, // 10 MHz
         .mode = 0,                          // SPI mode 0
-        .spics_io_num = PIN_NUM_CS,
+        .clock_speed_hz = 2 * 1000 * 1000, // 10 MHz
+
+        .spics_io_num = (gpio_num_t)PIN_NUM_CS,
         .queue_size = 1,
     };
 
     // Initialize SPI bus and device
-    gpio_reset_pin(PIN_NUM_MOSI);
-    gpio_reset_pin(PIN_NUM_CLK);
-    gpio_reset_pin(PIN_NUM_CS);
+    gpio_reset_pin((gpio_num_t)PIN_NUM_MOSI);
+    gpio_reset_pin((gpio_num_t)PIN_NUM_CLK);
+    gpio_reset_pin((gpio_num_t)PIN_NUM_CS);
     spi_bus_initialize(HSPI_HOST, &buscfg, SPI_DMA_CH_AUTO);
     spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
     // gpio_set_direction(PIN_NUM_MOSI, GPIO_MODE_OUTPUT);
@@ -76,7 +89,7 @@ void app_main(void) {
                     .tx_buffer = &data[i],
                 };
                 spi_device_transmit(spi, &t); // blocking
-                 vTaskDelay(pdMS_TO_TICKS(500));  // Delay for 3 seconds
+                 vTaskDelay(pdMS_TO_TICKS(100));  // Delay for 3 seconds
             }
 
         // }
