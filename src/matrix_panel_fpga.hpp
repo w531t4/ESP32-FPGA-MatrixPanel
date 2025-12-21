213e83acbeb58b3456bea8b9ca010003d4c2ac18
Aaron White <w531t4@gmail.com>
2025-12-21 11:33:28 -0500

    update ack. update mrcodetastic name

--- src/matrix_panel_fpga.hpp
+++ src/matrix_panel_fpga.hpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2018-2032 Faptastic
+// SPDX-FileCopyrightText: 2018-2032 mrcodetastic
 // SPDX-FileCopyrightText: 2025 Aaron White <w531t4@gmail.com>
 // SPDX-License-Identifier: MIT
 #ifndef _ESP32_RGB_64_32_MATRIX_PANEL_SPI_DMA
