7c2bfd71da5ec9fea458193d287858aafd07c239
Aaron White <w531t4@gmail.com>
2025-12-22 23:48:51 +0000

    fix scripts

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -2,12 +2,8 @@
 # SPDX-FileCopyrightText: 2025 Aaron White <w531t4@gmail.com>
 # SPDX-License-Identifier: MIT
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
-
 if [ ! -d /workspace ]; then
     cd ${SCRIPT_PATH}/../toolchain/esp-idf/
     . ./export.sh
 fi
-rm -f log.txt || true
-timeout --foreground  5s idf.py -p /dev/ttyUSB0 monitor 1> log.txt
-reset
-grep "Backtrace: " log.txt
+esptool.py --baud 1152000 write_flash -z 0x0000 /tmp/firmware.bin
